0465bf3938a7a14fe2c7c8646d3380afaf4d1fef
prerana2601 <155650129+prerana2601@users.noreply.github.com>
2024-11-25 12:28:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+// Function to display the length of the current URL
+function displayUrlLength() {
+    // Get the current URL from the location object
+    var currentUrl = window.location.href;
+    
+    // Get the length of the URL
+    var urlLength = currentUrl.length;
+    
+    // Show the URL length in an alert box
+    alert("The length of the URL is: " + urlLength);
+}
